9ecdc150e2e9501e0f3bacd62f4a02b9e1dab8d6
josel <jos23limbert@gmail.com>
2025-09-08 18:08:31 -0400

    correcion transacciones para creacion

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,18 +1,24 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types, UpdateQuery } from 'mongoose';
+import {
+  Injectable,
+  HttpException,
+  NotFoundException,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model, Types, UpdateQuery } from 'mongoose';
 
-import { CreateMessageDto } from './dtos';
-import { PaginationDto } from '../common';
-import { Chat, Message } from './schemas';
-import { User } from '../users/schemas';
-import { Account } from '../administration/schemas';
 import { FilesService } from '../files/files.service';
 import { FileGroup } from '../files/file-group.enum';
-
+import { Account } from '../administration/schemas';
+import { PaginationDto } from '../common';
+import { CreateMessageDto } from './dtos';
+import { Chat, Message } from './schemas';
+import { User } from '../users/schemas';
 @Injectable()
 export class ChatService {
   constructor(
+    @InjectConnection() private connection: Connection,
     @InjectModel(Chat.name) private chatModel: Model<Chat>,
     @InjectModel(Message.name) private messageModel: Model<Message>,
     @InjectModel(Account.name) private accountModel: Model<Account>,
@@ -75,53 +81,71 @@ export class ChatService {
 
     if (!chat) throw new NotFoundException(`Chat id ${chatId} not found`);
 
-    const newMessage = new this.messageModel({
-      sender: sender.id,
-      chat: chat.id,
-      readBy: [sender.id],
-      ...messageDto,
-    });
+    const session = await this.connection.startSession();
 
-    await newMessage.save();
+    try {
+      session.startTransaction();
 
-    await this.messageModel.populate(newMessage, { path: 'sender', select: 'fullname' });
+      const newMessage = new this.messageModel({
+        chat: chat.id,
+        sender: sender.id,
+        readBy: [sender.id],
+        ...messageDto,
+      });
 
-    const updateQuery: UpdateQuery<Chat> = {
-      lastActivity: new Date(),
-      $inc: { 'participants.$[item].unreadCount': 1 },
-      hasMessages: true,
-      lastMessage: {
-        ref: newMessage._id,
-        sender: newMessage.sender,
-        sentAt: newMessage.sentAt,
-        senderName: sender.fullname,
-        isRead: false,
-        type: newMessage.type,
-        content: newMessage.type === 'text' ? newMessage.content : newMessage.media.originalName ?? '',
-      },
-    };
+      newMessage.save({ session });
 
-    const createdChat = await this.chatModel
-      .findByIdAndUpdate(chatId, updateQuery, { arrayFilters: [{ 'item.user': { $ne: sender.id } }], new: true })
-      .populate({ path: 'participants.user', select: 'fullname' });
+      await this.messageModel.populate(newMessage, { path: 'sender', select: 'fullname', options: { session } });
 
-    const plainMessage = this.plainMessage(newMessage, chat, sender);
+      const updateQuery: UpdateQuery<Chat> = {
+        $inc: { 'participants.$[item].unreadCount': 1 },
+        hasMessages: true,
+        lastActivity: newMessage.sentAt,
+        lastMessage: {
+          ref: newMessage._id,
+          sender: newMessage.sender,
+          sentAt: newMessage.sentAt,
+          senderName: sender.fullname,
+          isRead: false,
+          type: newMessage.type,
+          content: newMessage.type === 'text' ? newMessage.content : newMessage.media.originalName ?? '',
+        },
+      };
 
-    return {
-      chatForMe: {
-        chat: this.plainChat(sender, createdChat),
-        message: plainMessage,
-      },
-      chatForOthers: createdChat.participants
-        .filter(({ user }) => String(user._id) !== sender.id)
-        .map(({ user }) => ({
-          toUser: String(user._id),
-          payload: {
-            chat: this.plainChat(user, createdChat),
-            message: plainMessage,
-          },
-        })),
-    };
+      const createdChat = await this.chatModel
+        .findByIdAndUpdate(chatId, updateQuery, {
+          arrayFilters: [{ 'item.user': { $ne: sender.id } }],
+          new: true,
+          session,
+        })
+        .populate({ path: 'participants.user', select: 'fullname' });
+
+      await session.commitTransaction();
+
+      const plainMessage = this.plainMessage(newMessage, chat, sender);
+
+      return {
+        chatForMe: {
+          chat: this.plainChat(sender, createdChat),
+          message: plainMessage,
+        },
+        chatForOthers: createdChat.participants
+          .filter(({ user }) => String(user._id) !== sender.id)
+          .map(({ user }) => ({
+            toUser: String(user._id),
+            payload: {
+              chat: this.plainChat(user, createdChat),
+              message: plainMessage,
+            },
+          })),
+      };
+    } catch (error) {
+      if (session.inTransaction()) await session.abortTransaction();
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Error send message');
+    } finally {
+      await session.endSession();
+    }
   }
 
   async markChatAsRead(chatId: string, user: User) {
@@ -129,40 +153,59 @@ export class ChatService {
 
     if (!chat) throw new NotFoundException(`Chat ${chatId} not found`);
 
-    // * Mark messages as read and insert readers in array
-    await this.messageModel.updateMany(
-      {
-        chat: chatId,
-        sender: { $ne: user.id },
-        readBy: { $ne: user.id },
-      },
-      { $addToSet: { readBy: user.id } },
-    );
+    const session = await this.connection.startSession();
 
-    // * After update chats, populate ref for get updated readBy
-    await this.chatModel.populate<{ 'lastMessage.ref': Message }>(chat, { path: 'lastMessage.ref', select: 'readBy' });
+    try {
+      session.startTransaction();
 
-    let isLastMessageRead = false;
+      // * Mark messages as read and insert readers in array
+      await this.messageModel.updateMany(
+        {
+          chat: chatId,
+          sender: { $ne: user.id },
+          readBy: { $ne: user.id },
+        },
+        { $addToSet: { readBy: user.id } },
+        { session },
+      );
 
-    if (chat.lastMessage) {
-      const lastMessageRef = chat.lastMessage.ref;
-      const readerIds = lastMessageRef['readBy'] as Types.ObjectId[];
-      isLastMessageRead = readerIds.length === chat.participants.length;
+      // * After update chats, populate ref for get updated readBy
+      await this.chatModel.populate<{ 'lastMessage.ref': Message }>(chat, {
+        path: 'lastMessage.ref',
+        select: 'readBy',
+        options: { session },
+      });
+
+      let isLastMessageRead = false;
+
+      if (chat.lastMessage) {
+        const lastMessageRef = chat.lastMessage.ref;
+        const readerIds = lastMessageRef['readBy'] as Types.ObjectId[];
+        isLastMessageRead = readerIds.length === chat.participants.length;
+      }
+
+      const updateChatQuery: UpdateQuery<Chat> = {
+        $set: {
+          'participants.$[item].unreadCount': 0,
+          ...(chat.lastMessage && { 'lastMessage.isRead': isLastMessageRead }),
+        },
+      };
+
+      await this.chatModel.updateOne({ _id: chatId }, updateChatQuery, { arrayFilters: [{ 'item.user': user.id }] });
+
+      await session.commitTransaction();
+
+      return {
+        message: 'Chat marked as read successfully',
+        participantId: chat.participants.map(({ user }) => String(user._id)).filter((id) => id !== user.id),
+      };
+    } catch (error) {
+      if (session.inTransaction()) await session.abortTransaction();
+      if (error instanceof HttpException) throw error;
+      throw new InternalServerErrorException('Error send message');
+    } finally {
+      await session.endSession();
     }
-
-    const updateChatQuery: UpdateQuery<Chat> = {
-      $set: {
-        'participants.$[item].unreadCount': 0,
-        ...(chat.lastMessage && { 'lastMessage.isRead': isLastMessageRead }),
-      },
-    };
-
-    await this.chatModel.updateOne({ _id: chatId }, updateChatQuery, { arrayFilters: [{ 'item.user': user.id }] });
-
-    return {
-      message: 'Chat marked as read successfully',
-      participantId: chat.participants.map(({ user }) => String(user._id)).filter((id) => id !== user.id),
-    };
   }
 
   private plainChat(currentUser: User, chat: Chat) {
